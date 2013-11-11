6997e18ce3b04438c980b3d3ef3df65e2ce18171
Knut Remi Lovli <knutremi@myopera.com>
2013-11-11 17:55:24 +0100

    No capitalization for non-TimeEdit columns.

--- lecture_feedback.sql
+++ lecture_feedback.sql
@@ -1,49 +1,53 @@
--- Table structure for table `Comments`
-CREATE TABLE IF NOT EXISTS `Comments` (
+/* The column names that start with capitalization are using the English name
+ * of the key from the TimeEdit data.
+ */
+
+-- Table structure for table `comments`
+CREATE TABLE IF NOT EXISTS `comments` (
   `Room` char(30) NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT '0000-00-00 00:00',
-  `Student_hash` binary(20) NOT NULL,
-  `Student_comment` varchar(333) NOT NULL,
-  `Comment_flags` smallint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`Room`,`Time`,`Student_hash`,`Student_comment`)
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00',
+  `student_hash` binary(20) NOT NULL,
+  `student_comment` varchar(333) NOT NULL,
+  `comment_flags` smallint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Room`,`time`,`student_hash`,`student_comment`)
 );
 
--- Table structure for table `Feedback_names`
-CREATE TABLE IF NOT EXISTS `Feedback_names` (
-  `Feedback_name` varchar(50) NOT NULL,
-  `Active` tinyint(1) NOT NULL,
-  PRIMARY KEY (`Feedback_name`)
+-- Table structure for table `feedback_names`
+CREATE TABLE IF NOT EXISTS `feedback_names` (
+  `feedback_name` varchar(50) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`feedback_name`)
 );
 
--- Table structure for table `Lecture`
-CREATE TABLE IF NOT EXISTS `Lecture` (
+-- Table structure for table `lecture`
+CREATE TABLE IF NOT EXISTS `lecture` (
   `Room` varchar(30) NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT '0000-00-00 00:00',
-  `Duration` int(4) unsigned NOT NULL,
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00',
+  `duration` int(4) unsigned NOT NULL,
   `Subject` int(11) NOT NULL,
   `Teacher` int(11) NOT NULL,
-  PRIMARY KEY (`Room`,`Time`)
+  PRIMARY KEY (`Room`,`time`)
 );
 
--- Table structure for table `Values`
-CREATE TABLE IF NOT EXISTS `Values` (
+-- Table structure for table `values`
+CREATE TABLE IF NOT EXISTS `values` (
   `Room` char(30) NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT '0000-00-00 00:00',
-  `Student_hash` binary(20) NOT NULL,
-  `Feedback_name` varchar(50) NOT NULL,
-  `Feedback_value` tinyint(1) NOT NULL,
-  PRIMARY KEY (`Room`,`Time`,`Student_hash`,`Feedback_name`),
-  KEY `Feedback_name` (`Feedback_name`)
+  `time` timestamp NOT NULL DEFAULT '0000-00-00 00:00',
+  `student_hash` binary(20) NOT NULL,
+  `feedback_name` varchar(50) NOT NULL,
+  `feedback_value` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Room`,`time`,`student_hash`,`feedback_name`),
+  KEY `feedback_name` (`feedback_name`)
 );
 
--- Constraints for table `Comments`
-ALTER TABLE `Comments`
-  ADD CONSTRAINT `Comments_Lecture_FK` FOREIGN KEY (`Room`, `Time`) REFERENCES `Lecture` (`Room`, `Time`);
+-- Constraints for table `comments`
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_lecture_FK` FOREIGN KEY (`Room`, `time`) REFERENCES `lecture` (`Room`, `time`);
 
--- Constraints for table `Values`
-ALTER TABLE `Values`
-  ADD CONSTRAINT `Values_Lecture_FK` FOREIGN KEY (`Room`, `Time`) REFERENCES `Lecture` (`Room`, `Time`);
+-- Constraints for table `values`
+ALTER TABLE `values`
+  ADD CONSTRAINT `Values_Lecture_FK` FOREIGN KEY (`Room`, `time`) REFERENCES `lecture` (`Room`, `time`);
   
--- Constraints for table `Values`
-ALTER TABLE `Values`
-  ADD CONSTRAINT `Values_Feedback_Names_FK` FOREIGN KEY (`Feedback_name`) REFERENCES `Feedback_names` (`Feedback_name`);
+-- Constraints for table `values`
+ALTER TABLE `values`
+  ADD CONSTRAINT `values_feedback_names_FK` FOREIGN KEY (`feedback_name`) REFERENCES `feedback_names` (`feedback_name`);
