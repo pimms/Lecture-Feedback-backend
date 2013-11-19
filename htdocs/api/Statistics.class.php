8dac76f75ef085f83e7933a2227382a970d9e80f
Joakim Stien <joakim.stien@gmail.com>
2013-11-19 22:53:05 +0100

    Added support for lecture filteringgit add .git add .

--- htdocs/api/Statistics.class.php
+++ htdocs/api/Statistics.class.php
@@ -23,22 +23,40 @@ class Statistics {
 	 * {"positive":N,"negative":M}
 	 */
 	public static function getTotalVotesForCourse($course) {
-		// We get the number of positive votes by removing
-		// all the "1"'s from the stringified ratings, and
-		// we get the amount of total votes by multiplying 
-		// the times we replace the ones with the number of
-		// attributes.
-		$count = NUM_ATTRIBUTES;
-		$query = "SELECT SUM(len) AS positive, "
-				." 		 COUNT(*) * {$count} as total "
-				."FROM ( "
-				."	SELECT "
-				."		LENGTH(ratings) - "
-				." 		LENGTH(REPLACE(ratings,'1','')) "
-				."		AS len "
-				."	FROM ReviewItem "
-				." WHERE courseCode='{$course}' "
-				.")T";
+		$where = "WHERE course='{$course}'";
+		return self::getTotalVotes($course);
+	}
+
+
+	/**
+	 * Get the total number of votes from a lecture.
+	 *
+	 * @param hash
+	 * The lecture hash.
+	 *
+	 * @return
+	 * {"positive":M, "negative",N}
+	 */
+	public static function getTotalVotesForLecture($hash) {
+		$where = "WHERE hash='{$hash}'";
+		return self::getTotalVotes($where);
+	}
+
+
+
+	/**
+	 * Return the number of positive / negative votes for 
+	 * a set of lectures.
+	 * 
+	 * @param whereClause
+	 * The FULL WHERE-clause of the query, or null for 
+	 * unconditional querying. Example: "WHERE 1=1"
+	 *
+	 * @return
+	 * {"positive":M, "negative":N}
+	 */
+	private static function getTotalVotes($whereClause) {
+		$query = self::getVoteCountQuery($whereClause);
 		
 		Database::open();
 		$stmt = Database::query($query);
@@ -55,6 +73,39 @@ class Statistics {
 		$arr["positive"] = $pos;
 		$arr["negative"] = $neg;
 		return $arr;
+	}
+
+
+	/**
+	 * Returns the query that will retrieve the number of votes
+	 * for a set of lectures. Use the whereClause parameter to
+	 * define how the set is generated.
+	 *
+	 * @param whereClause
+	 * The FULL WHERE-clause of the query, or null for 
+	 * unconditional querying. Example: "WHERE 1=1"
+	 *
+	 * @param return
+	 * Queryyyyy
+	 */
+	private static function getVoteCountQuery($whereClause) {
+		// We get the number of positive votes by removing
+		// all the "1"'s from the stringified ratings, and
+		// we get the amount of total votes by multiplying 
+		// the times we replace the ones with the number of
+		// attributes.
+		$count = NUM_ATTRIBUTES;
+		$query = "SELECT SUM(len) AS positive, "
+				." 		 COUNT(*) * {$count} as total "
+				."FROM ( "
+				."	SELECT "
+				."		LENGTH(ratings) - "
+				." 		LENGTH(REPLACE(ratings,'1','')) "
+				."		AS len "
+				."	FROM ReviewItem "
+				." {$whereClause} "
+				.")T";
+		return $query;
 	}
 }
 
--- htdocs/api/getStats.php
+++ htdocs/api/getStats.php
@@ -16,7 +16,10 @@ $action = getFromAssoc($_GET, "action", false) or dieBad();
 
 if ($action == "course_votes") {
 	$course = getFromAssoc($_GET, "course", false) or dieBad();
-	$json[$course] = Statistics::getTotalVotesForCourse($course);
+	$json = array_merge($json, Statistics::getTotalVotesForCourse($course));
+} else if ($action == "lecture_votes") {
+	$hash = getFromAssoc($_GET, "hash", false) or dieBad();
+	$json = array_merge($json, Statistics::getTotalVotesForLecture($hash));
 } else {
 	dieBad();
 }
