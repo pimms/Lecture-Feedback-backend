985b4aed5b6f345789b1c4ed80cbc94af5c2babd
Joakim Stien <joakim.stien@gmail.com>
2013-11-24 17:19:41 +0100

    wtf, it's like I'm drunk

--- htdocs/api/vote.php
+++ htdocs/api/vote.php
@@ -15,7 +15,7 @@ function dieBad($reason) {
 /* Fetch the required parameters or die horribly */
 $reviewId = getFromAssoc($_GET, "review_id", null) 
 			or dieBad("review_id is not defined");
-$vote_type = getFromAssic($_GET, "type", null)
+$vote_type = getFromAssoc($_GET, "type", null)
 			or dieBad("type is not defined");
 
 if ($vote_type != "clone" && $vote_type != "down") {
