febb45f8cfa819ea5fe68b7da1e3120fc16508c8
Joakim Stien <joakim.stien@gmail.com>
2013-12-03 13:09:39 +0100

    Added lastid parameter to getfeed

--- htdocs/api/Utils.php
+++ htdocs/api/Utils.php
@@ -24,4 +24,59 @@ function getFromAssoc($assoc, $key, $default) {
 	return isset($assoc[$key]) ? $assoc[$key] : $default;
 }
 
+
+function prettyPrint( $json )
+{
+    $result = '';
+    $level = 0;
+    $prev_char = '';
+    $in_quotes = false;
+    $ends_line_level = NULL;
+    $json_length = strlen( $json );
+
+    for( $i = 0; $i < $json_length; $i++ ) {
+        $char = $json[$i];
+        $new_line_level = NULL;
+        $post = "";
+        if( $ends_line_level !== NULL ) {
+            $new_line_level = $ends_line_level;
+            $ends_line_level = NULL;
+        }
+        if( $char === '"' && $prev_char != '\\' ) {
+            $in_quotes = !$in_quotes;
+        } else if( ! $in_quotes ) {
+            switch( $char ) {
+                case '}': case ']':
+                    $level--;
+                    $ends_line_level = NULL;
+                    $new_line_level = $level;
+                    break;
+
+                case '{': case '[':
+                    $level++;
+                case ',':
+                    $ends_line_level = $level;
+                    break;
+
+                case ':':
+                    $post = " ";
+                    break;
+
+                case " ": case "\t": case "\n": case "\r":
+                    $char = "";
+                    $ends_line_level = $new_line_level;
+                    $new_line_level = NULL;
+                    break;
+            }
+        }
+        if( $new_line_level !== NULL ) {
+            $result .= "<br/>".str_repeat( "&nbsp;", 4*$new_line_level );
+        }
+        $result .= $char.$post;
+        $prev_char = $char;
+    }
+
+    return $result;
+}
+
 ?>
--- htdocs/api/getFeed.php
+++ htdocs/api/getFeed.php
@@ -4,76 +4,20 @@ require_once("ReviewFeed.class.php");
 require_once("Utils.php");
 
 
-function prettyPrint( $json )
-{
-    $result = '';
-    $level = 0;
-    $prev_char = '';
-    $in_quotes = false;
-    $ends_line_level = NULL;
-    $json_length = strlen( $json );
-
-    for( $i = 0; $i < $json_length; $i++ ) {
-        $char = $json[$i];
-        $new_line_level = NULL;
-        $post = "";
-        if( $ends_line_level !== NULL ) {
-            $new_line_level = $ends_line_level;
-            $ends_line_level = NULL;
-        }
-        if( $char === '"' && $prev_char != '\\' ) {
-            $in_quotes = !$in_quotes;
-        } else if( ! $in_quotes ) {
-            switch( $char ) {
-                case '}': case ']':
-                    $level--;
-                    $ends_line_level = NULL;
-                    $new_line_level = $level;
-                    break;
-
-                case '{': case '[':
-                    $level++;
-                case ',':
-                    $ends_line_level = $level;
-                    break;
-
-                case ':':
-                    $post = " ";
-                    break;
-
-                case " ": case "\t": case "\n": case "\r":
-                    $char = "";
-                    $ends_line_level = $new_line_level;
-                    $new_line_level = NULL;
-                    break;
-            }
-        }
-        if( $new_line_level !== NULL ) {
-            $result .= "<br/>".str_repeat( "&nbsp;", 4*$new_line_level );
-        }
-        $result .= $char.$post;
-        $prev_char = $char;
-    }
-
-    return $result;
-}
-
-
-
 $json = array("status" => "bad");
 
 
 if (isset($_GET["filter"]) || isset($_GET["hash"])) {
 	// Set FIRST and COUNT from default / GET
-	$first = getFromAssoc($_GET, "first", 0);
-	$count = getFromAssoc($_GET, "count", 25);
-
+	$first  = getFromAssoc($_GET, "first", 0);
+    $lastId = getFromAssoc($_GET, "lastid", 0);
+	$count  = getFromAssoc($_GET, "count", 25);
+    $hash   = getFromAssoc($_GET, "hash", null);
     $filter = getFromAssoc($_GET, "filter", null);
+
     if ($filter) {
         $filter = explode(",", $_GET["filter"]);
     }
-
-    $hash = getFromAssoc($_GET, "hash", null);
 
 	// Retrieve the feed of items
 	$feed = new ReviewFeed();
